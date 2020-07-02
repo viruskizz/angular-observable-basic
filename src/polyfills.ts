301aba1ced9c281e9e3fb1399f9a8ef6edc5c836
Araiva Viruskizz <kizzaraiva@gmail.com>
2020-07-02 22:33:38 +0700

    fix conflict

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -20,6 +20,7 @@
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'core-js/es/reflect';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
@@ -61,3 +62,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
